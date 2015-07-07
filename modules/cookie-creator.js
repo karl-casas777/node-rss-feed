97566bb932e13869a8cccfc1e4dab72ac351a387
krain143 <karl.casas777@gmail.com>
2015-07-07 12:14:06 +0800

    cookie creator check validity changed

--- modules/cookie-creator.js
+++ modules/cookie-creator.js
@@ -1,6 +1,6 @@
 var cookieCreator = function(req, res, next) {
   var cookie = req.cookies.rssapp;
-  if (cookie === undefined)
+  if (!cookie)
   {
     var uniqnum = Math.random().toString();
     uniqnum = uniqnum.substring(2, uniqnum.length);
